38dd8ec748fcadf369f32d863bc82d21bd4f5937
Hítalo Rodrigues <hitalo005@gmail.com>
2024-01-11 13:47:39 -0300

    style: styles post

--- src/components/post/post.jsx
+++ src/components/post/post.jsx
@@ -14,16 +14,20 @@ export function Post(props) {
             <strong>Diego Fernandes</strong>
             <span>Web Developer</span>
           </div>
-          <time title="11 de Janeiro de 2024 " dateTime='11/01/2024 12:00:00'>Publicado há 1h</time>
         </div>
+        <time title="11 de Janeiro de 2024 " dateTime='11/01/2024 12:00:00'>Publicado há 1h</time>
       </header>
       <div className={styles.content}>
         <p>Fala galeraa 👋 </p><br/>
         <p>
           Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
         </p><br/>
-        <p> <a href="">👉 jane.design/doctorcare</a></p>
-        <p> <a href="">#novoprojeto #nlw #rocketseat</a></p>
+        <p>👉{' '}<a href="">jane.design/doctorcare</a></p>
+        <p> 
+          <a href="">#novoprojeto</a>{' '}
+          <a href="">#nlw</a>{' '}
+          <a href="">#rocketseat</a>
+        </p>
       </div>
     </article>
   )
